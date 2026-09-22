adeee4d943e2880eb44a6bbdc7b3b29d48cc248b
dhruvarora-ai <aroradhruv444@gmail.com>
2026-09-23 03:10:02 +0530

    Time: 8 ms (54.02%), Space: 22.9 MB (89.67%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    long long hrs(vector<int>& piles, int speed){
-        long long hrs = 0;
+    bool check(vector<int>& piles, int h, int speed){
+        int hrs = 0;
         for(int i=0; i<piles.size(); i++){
-            if(piles[i]%speed==0){
+            if(piles[i]<speed){
+                hrs+=1;
+            }
+            else if(piles[i]%speed==0){
                 hrs+= piles[i]/speed;
             }
             else{
-                hrs+= piles[i]/speed+1;
+                hrs+= piles[i]/speed + 1;
+            }
+            if(hrs>h){
+                return false;
             }
         }
-        return hrs;
+        return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low=1;
-        int high = *max_element(piles.begin(),piles.end());
-        long long ans=0;
-        while(low<=high){
-            long long mid = (low+high)/2;
-            long long hours = hrs(piles,mid);
-            if(hours<=h){
-                ans = mid;
-                high = mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+      int low=1;
+      int high = *max_element(piles.begin(),piles.end());
+      int speed=0;
+      while(low<=high){
+        int mid = (low+high)/2;
+        bool possible = check(piles, h,mid);
+        if(possible){
+            speed=mid;
+            high = mid-1;
         }
-        return ans;
+        else{
+            low=mid+1;
+        }
+      }
+      return speed;  
     }
 };
